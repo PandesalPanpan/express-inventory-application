855c27e9e42b22bbca65e45beb4a268b286f7342
PandesalPanpan <petermarticio@gmail.com>
2025-11-17 17:25:19 +0800

    Feat: Move department/create routes to be checked first before params routes

--- routes/departmentRouter.js
+++ routes/departmentRouter.js
@@ -4,10 +4,10 @@ import * as departmentController from '../controllers/departmentController.js';
 const departmentRouter = Router();
 
 departmentRouter.get('/', departmentController.getAllDepartments);
-departmentRouter.get('/:departmentId/rooms', departmentController.getRoomsByDepartment);
-departmentRouter.get('/:departmentId', departmentController.getDepartment);
 departmentRouter.get('/create', departmentController.createDepartmentGet);
 departmentRouter.post('/create', departmentController.createDepartmentPost);
+departmentRouter.get('/:departmentId/rooms', departmentController.getRoomsByDepartment);
+departmentRouter.get('/:departmentId', departmentController.getDepartment);
 departmentRouter.post('/:departmentId/update', departmentController.updateDepartmentPost);
 departmentRouter.delete('/:departmentId/delete', departmentController.deleteDepartment);
 
